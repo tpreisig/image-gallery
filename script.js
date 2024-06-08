e7247f6852df1d66d301565a05ca092a4758ca29
tpreisig <tiziano.preisig@hispeed.ch>
2024-06-08 23:52:46 +0200

    Updated stylesheet

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+const imagesWrapper = document.querySelector(".images");
+const morePicsBtn = document.querySelector(".load-more");
+const searchInput = document.querySelector(".search-box input");
+const lightBox = document.querySelector(".lightbox");
+const closeBtn = lightBox.querySelector("#close");
+const downloadImgBtn = lightBox.querySelector("#download");
+
+
+const showLightbox = (name, img) => {
+    lightBox.querySelector("img").src = img;
+}
+
+
+closeBtn.addEventListener('click', function(){
+    lightBox.classList.remove("show");
+    document.body.style.overflow = "auto";
+})
+
+
+
+
